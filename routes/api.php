8b17bbc4af066a05cdb0eea6094e826fcf08a15e
topdev00000 <adrian.gortopan@gmail.com>
2017-04-18 20:34:10 +0900

    fix/api_integration_with_laravel

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\ReportController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReportController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
