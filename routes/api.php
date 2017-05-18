81e2ce29d1ca5e9388142535c89703bdb9eb81b6
topdev00000 <adrian.gortopan@gmail.com>
2017-05-18 20:40:07 +0900

    fix/complete version

--- routes/api.php
+++ routes/api.php
@@ -9,16 +9,7 @@ use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\ReportController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
 
 Route::middleware(['auth:sanctum', 'admin'])->group(function () {
     Route::get('/user', [\App\Http\Controllers\Api\AuthController::class, 'getUser']);
