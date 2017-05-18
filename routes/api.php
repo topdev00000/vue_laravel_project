623b009f898500c32d998a3242b43dd25c1561e7
topdev00000 <adrian.gortopan@gmail.com>
2017-05-18 20:35:49 +0900

    complete_project

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,6 @@ use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\ReportController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ReportController;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
